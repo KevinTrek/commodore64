68238ec949e5f3668f1cf70b23b1a401511d4e0a
Kevin Cox <outcastpixel@users.noreply.github.com>
2016-05-11 04:09:31 -0500

    Update test02.asm

--- test02.asm
+++ test02.asm
@@ -3,7 +3,7 @@
 ; Assembled with DASM
 ; I've seen this before, but I'll give credit to
 ; https://digitalerr0r.wordpress.com/2011/03/19/commodore-64-programming-a-quick-start-guide-to-c-64-assembly-programming-on-windows/
-		processor 6502
-		org	$1000
+	processor 6502
+	org	$1000
 loop: 	inc $d020
-		jmp loop
+	jmp loop
